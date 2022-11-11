4632fb3dec23ab13505a848ea3766d5962be2679
Roni Wellington <roniwellington@gmail.com>
2022-11-11 15:52:09 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
-//mais um console log de teste
+let subindoArquivo = "mais um de teste";
+console.log(subindoArquivo)
 console.log("O paulo fez esse commit sem você ver na aula!")
